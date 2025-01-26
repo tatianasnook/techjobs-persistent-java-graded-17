e18f2118e29eb23e6cddb33263f66a96e77632eb
Tatiana Snook <tatsnook@gmail.com>
2025-01-26 17:25:33 -0600

    task2: complete EmployerController and write SkillController

--- queries.sql
+++ queries.sql
@@ -1,7 +1,11 @@
 --Part 1
+id int PK
+employer varchar(255)
+name varchar(255)
+skills varchar(255)
 
 --Part 2
-
+SELECT name from employer WHERE location = "St. Louis City";
 --Part 3
 
 --Part 4
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,28 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "Location is required.")
+    @Size(min = 3, max = 150, message = "Location must be between 3 and 150 characters.")
+    private String location;
+
+    public Employer() {}
+
+    public Employer(String location) {
+        super();
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
